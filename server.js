9e24e4f8ff265bc572be6d3792caa545c396fabe
Josh Summerhays <russiansummer@gmail.com>
2016-12-12 12:44:14 -0700

    BUILD: prep for deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-let express = require('express');
+const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -64,6 +65,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000');
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
 });
